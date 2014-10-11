cf15f44aeaabc8e64f9b5c5e7fc454d9362dc256
halaei <hamid.a85@gmail.com>
2014-10-11 15:58:06 +0330

    presentableTrait can work with both static and instance variables: presenter_class, presenter_callables, and presenter_friend

--- src/Halaei/Presenter/PresentableTrait.php
+++ src/Halaei/Presenter/PresentableTrait.php
@@ -4,9 +4,9 @@ trait PresentableTrait {
 
     public function present()
     {
-        $presenter_class = isset(static::$presenter_class) ? static::$presenter_class: 'Halaei\Presenter\BasePresenter';
-        $presenter_callables = isset(static::$presenter_callables) ? static::$presenter_callables: [];
-        $presenter_friend = isset(static::$presenter_friend) ? static::$presenter_friend: null;
+        $presenter_class = isset($this->presenter_class) ? $this->presenter_class: 'Halaei\Presenter\BasePresenter';
+        $presenter_callables = isset($this->presenter_callables) ? $this->presenter_callables: [];
+        $presenter_friend = isset($this->presenter_friend) ? $this->presenter_friend: null;
         return new $presenter_class($this, $presenter_callables, $presenter_friend);
     }
 } 
